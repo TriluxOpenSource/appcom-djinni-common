f33da18cb1633a1fab757613ce9d1ca835b66b65
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2017-08-24 15:50:22 +0200

    added zip and deploy for nexus

--- build-android.sh
+++ build-android.sh
@@ -5,6 +5,13 @@ declare NDK_PATH='/Users/gordon/Library/Android/sdk/ndk-bundle'
 declare TOOLCHAIN_VERSION=clang
 declare STL_TYPE=c++_static
 
+declare COMMIT=$(git rev-list --tags --max-count=1)
+declare VERSION=$(git describe --tags ${COMMIT})
+declare OUTPUT_FILE=ac-ms-common-sdk-android-${VERSION}.zip
+
+# set to TRUE to deploy to Nexus
+declare DEPLOY_TO_NEXUS=FALSE
+
 # ======================================================================================================================
 
 cd $( dirname "${BASH_SOURCE[0]}" )
@@ -54,12 +61,28 @@ build_android 21 x86_64
 #
 # zip final result
 
-declare COMMIT=$(git rev-list --tags --max-count=1)
-declare VERSION=$(git describe --tags ${COMMIT})
-
 cd output
 
-zip -r ac-ms-common-sdk-android-${VERSION}.zip include lib java *.yml
+zip -r ${OUTPUT_FILE} include lib java *.yml
 
 # cleanup
-rm -r include lib *.yml
+rm -r include lib java *.yml
+
+# ======================================================================================================================
+#
+# upload to nexus
+
+if [ "${DEPLOY_TO_NEXUS}" = "TRUE" ]; then
+
+    # check if maven is installed
+    command -v mvn >/dev/null 2>&1 || { echo >&2 "Maven 2 is required but it's not installed. Aborting."; exit 1; }
+
+    mvn deploy:deploy-file -e \
+    -DgroupId=ac-ms-common-sdk \
+    -DartifactId=android \
+    -Dversion=${VERSION} \
+    -DgeneratePom=true \
+    -DrepositoryId=appcom-nexus \
+    -Durl=http://appcom-nexus/nexus/content/repositories/appcom-microservice-sdks \
+    -Dfile=${OUTPUT_FILE}
+fi
--- build-ios.sh
+++ build-ios.sh
@@ -6,6 +6,13 @@ declare IOS_TOOLCHAIN='cmake/ios.toolchain.cmake'
 # possible platforms: OS, SIMULATOR
 declare IOS_PLATFORM='OS'
 
+declare COMMIT=$(git rev-list --tags --max-count=1)
+declare VERSION=$(git describe --tags ${COMMIT})
+declare OUTPUT_FILE=ac-ms-common-sdk-ios-${VERSION}.zip
+
+# set to TRUE to deploy to Nexus
+declare DEPLOY_TO_NEXUS=FALSE
+
 # ======================================================================================================================
 
 cd $( dirname "${BASH_SOURCE[0]}" )
@@ -36,12 +43,28 @@ rm -rf build
 #
 # zip final result
 
-declare COMMIT=$(git rev-list --tags --max-count=1)
-declare VERSION=$(git describe --tags ${COMMIT})
-
 cd output
 
-zip -r ac-ms-common-sdk-ios-${VERSION}.zip include lib *.yml
+zip -r ${OUTPUT_FILE} include lib *.yml
 
 # cleanup
 rm -r include lib *.yml
+
+# ======================================================================================================================
+#
+# upload to nexus
+
+if [ "${DEPLOY_TO_NEXUS}" = "TRUE" ]; then
+
+    # check if maven is installed
+    command -v mvn >/dev/null 2>&1 || { echo >&2 "Maven 2 is required but it's not installed. Aborting."; exit 1; }
+
+    mvn deploy:deploy-file -e \
+    -DgroupId=ac-ms-common-sdk \
+    -DartifactId=ios \
+    -Dversion=${VERSION} \
+    -DgeneratePom=true \
+    -DrepositoryId=appcom-nexus \
+    -Durl=http://appcom-nexus/nexus/content/repositories/appcom-microservice-sdks \
+    -Dfile=${OUTPUT_FILE}
+fi
