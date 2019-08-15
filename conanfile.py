b55f23fc6fb2e2ecdaae7433224ccd6b3ce106cd
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2019-08-15 11:27:22 +0200

    fix ranlib

--- conanfile.py
+++ conanfile.py
@@ -20,50 +20,17 @@ class DjinniCommonConan(ConanFile):
         cmake = CMake(self)
         library_folder = self.source_folder
         cmake.verbose = True
-        variants = []
 
         self.run("./run-djinni.sh")
 
         if self.settings.os == "Android":
-            android_toolchain = os.environ["ANDROID_NDK_PATH"] + "/build/cmake/android.toolchain.cmake"
-            cmake.definitions["CMAKE_SYSTEM_NAME"] = "Android"
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = android_toolchain
-            cmake.definitions["ANDROID_NDK"] = os.environ["ANDROID_NDK_PATH"]
-            cmake.definitions["ANDROID_ABI"] = tools.to_android_abi(self.settings.arch)
-            cmake.definitions["ANDROID_STL"] = self.options.android_stl_type
-            cmake.definitions["ANDROID_NATIVE_API_LEVEL"] = self.settings.os.api_level
-            cmake.definitions["ANDROID_TOOLCHAIN"] = "clang"
-            cmake.definitions["DJINNI_WITH_JNI"] = "ON"
+            self.applyCmakeSettingsForAndroid(cmake)
 
         if self.settings.os == "iOS":
-            ios_toolchain = "cmake-modules/Toolchains/ios.toolchain.cmake"
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = ios_toolchain
-            cmake.definitions["DJINNI_WITH_OBJC"] = "ON"
-            
-            # define all architectures for ios fat library
-            if "arm" in self.settings.arch:
-                variants = ["armv7", "armv7s", "armv8"]
-
-            # apply build config for all defined architectures
-            if len(variants) > 0:
-                archs = ""
-                for i in range(0, len(variants)):
-                    if i == 0:
-                        archs = tools.to_apple_arch(variants[i])
-                    else:
-                        archs += ";" + tools.to_apple_arch(variants[i])
-                cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = archs
-
-            if self.settings.arch == "x86":
-                cmake.definitions["IOS_PLATFORM"] = "SIMULATOR"
-            elif self.settings.arch == "x86_64":
-                cmake.definitions["IOS_PLATFORM"] = "SIMULATOR64"
-            else:
-                cmake.definitions["IOS_PLATFORM"] = "OS"
+            self.applyCmakeSettingsForiOS(cmake)
 
         if self.settings.os == "Macos":
-            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
-            cmake.definitions["DJINNI_WITH_OBJC"] = "ON"
+            self.applyCmakeSettingsFormacOS(cmake)
 
         # build static library if shared option is disabled
         cmake.definitions["BUILD_STATIC_LIB"] = "ON" if self.options.shared == False else "OFF"
@@ -72,14 +39,57 @@ class DjinniCommonConan(ConanFile):
         cmake.build()
         cmake.install()
 
-        lib_dir = os.path.join(self.package_folder, "lib")
+        # execute ranlib for all static universal libraries (required for fat libraries on iOS)
+        if self.settings.os == "iOS" and not self.options.shared:
+            self.runRanlibForiOS(os.path.join(self.package_folder, "lib"))
 
-        # execute ranlib for all static universal libraries (required for fat libraries)
-        if self.settings.os == "iOS" and len(variants) > 0:
-            if self.options.shared == False:
-                for f in os.listdir(lib_dir):
-                    if f.endswith(".a") and os.path.isfile(os.path.join(lib_dir,f)) and not os.path.islink(os.path.join(lib_dir,f)):
-                        self.run("xcrun ranlib %s" % os.path.join(lib_dir,f))
+    def applyCmakeSettingsForAndroid(self, cmake):
+        android_toolchain = os.environ["ANDROID_NDK_PATH"] + "/build/cmake/android.toolchain.cmake"
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = android_toolchain
+        cmake.definitions["ANDROID_NDK"] = os.environ["ANDROID_NDK_PATH"]
+        cmake.definitions["ANDROID_ABI"] = tools.to_android_abi(self.settings.arch)
+        cmake.definitions["ANDROID_STL"] = self.options.android_stl_type
+        cmake.definitions["ANDROID_NATIVE_API_LEVEL"] = self.settings.os.api_level
+        cmake.definitions["ANDROID_TOOLCHAIN"] = "clang"
+        cmake.definitions["DJINNI_WITH_JNI"] = "ON"
+
+    def applyCmakeSettingsForiOS(self, cmake):
+        variants = []
+        ios_toolchain = "cmake-modules/Toolchains/ios.toolchain.cmake"
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = ios_toolchain
+        cmake.definitions["DJINNI_WITH_OBJC"] = "ON"
+        
+        # define all architectures for ios fat library
+        if "arm" in self.settings.arch:
+            variants = ["armv7", "armv7s", "armv8"]
+
+        # apply build config for all defined architectures
+        if len(variants) > 0:
+            archs = ""
+            for i in range(0, len(variants)):
+                if i == 0:
+                    archs = tools.to_apple_arch(variants[i])
+                else:
+                    archs += ";" + tools.to_apple_arch(variants[i])
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = archs
+
+        if self.settings.arch == "x86":
+            cmake.definitions["IOS_PLATFORM"] = "SIMULATOR"
+        elif self.settings.arch == "x86_64":
+            cmake.definitions["IOS_PLATFORM"] = "SIMULATOR64"
+        else:
+            cmake.definitions["IOS_PLATFORM"] = "OS"
+
+    def applyCmakeSettingsFormacOS(self, cmake):
+        cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+        cmake.definitions["DJINNI_WITH_OBJC"] = "ON"
+
+    def runRanlibForiOS(self, lib_dir):
+        if self.settings.arch != "x86" and self.settings.arch != "x86_64":
+            for f in os.listdir(lib_dir):
+                if f.endswith(".a") and os.path.isfile(os.path.join(lib_dir,f)) and not os.path.islink(os.path.join(lib_dir,f)):
+                    print("xcrun ranlib %s" % os.path.join(lib_dir,f))
+                    self.run("xcrun ranlib %s" % os.path.join(lib_dir,f))
 
     def package(self):
         self.copy("*.h", dst="include", src='include')
